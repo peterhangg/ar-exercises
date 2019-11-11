84a1700edd81913e3199f3efc3f34b57c7454344
peterhangg <peterhanggg@gmail.com>
2019-11-11 23:35:20 +0000

    AR calculations

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+# refernce => Person.sum(:age)
+
+@sum = Store.sum(:annual_revenue)
+@average = Store.average(:annual_revenue)
+@stores_over_million = Store.where("annual_revenue > 1000000").count 
+
+puts "This is the total revenue of all stores: #{@sum}"
+puts "This is the average for each store: #{@average}"
+puts "Number of stores that generated over $1M in sales: #{@stores_over_million}"
