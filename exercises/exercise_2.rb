c48bbb98844695087dd1ae31114395b053094b30
peterhangg <peterhanggg@gmail.com>
2019-11-11 22:55:18 +0000

    updated the first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,10 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+# reference => user = User.find_by(name: 'David'), user.update(name: 'Dave')
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+@store1.update(name: "Sephora")
+puts @store1
+puts @store1.name   # => Sephora
