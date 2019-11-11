44fc7843f91668936c04f408d9e45745fc914721
peterhangg <peterhanggg@gmail.com>
2019-11-11 23:27:39 +0000

    loaded subset of stores active record query for getting men/women store

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,21 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+# reference => Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+# reference => Client.where(first_name: 'Lifo')
+
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |store|
+  puts "Men Store: #{store.name} and Annual revenue: #{store.annual_revenue}"
+end
+
+@womens_stores = Store.where("womens_apparel = true and annual_revenue < 1000000")
+
+@womens_stores.each do |store|
+  puts "Womens Store: #{store.name} and Annual revenue: #{store.annual_revenue}"
+end
